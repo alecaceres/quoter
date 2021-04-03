7ec6d7c20df084894fa60da5aa92f2a6052ee70a
Alejandro Cáceres <alejandro.caceres@wardenautomation.com>
2021-04-03 11:34:30 -0400

    Add styled components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,27 @@
 import React from "react";
+import styled from "@emotion/styled";
+
+const HeaderContainer = styled.header`
+    background-color: #26C6DA;
+    padding: 10px;
+    font-weight: bold;
+    color: #FFFFFF;
+
+`
+
+const HeaderText = styled.h1`
+    font-size: 2rem;
+    margin: 0;
+    font-family: 'Slabo 27px', serif;
+    text-align: center;
+`
 
 const Header = ({title}) => (
-    <header>
-        <h1>
+    <HeaderContainer>
+        <HeaderText>
             {title}
-        </h1>
-    </header>
+        </HeaderText>
+    </HeaderContainer>
 )
 
 export default Header;
